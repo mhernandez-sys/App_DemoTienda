deb55a3cc2f3f5edac0fbe4469b6aa63ba21a450
mhernandez-sys <mhernandez@garkcom.com.mx>
2024-08-09 11:05:48 -0600

    {MH} 47 commit main (En el bundle se proporciona la informacion para la lista) [09/08/2024-11:00]

--- app/src/main/java/com/example/demo/ui/Existencias/MenuExistenciasFragment.java
+++ app/src/main/java/com/example/demo/ui/Existencias/MenuExistenciasFragment.java
@@ -10,8 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.demo.R;
+import com.example.demo.ReciclerView.Productos.ListProductos;
 
 public class MenuExistenciasFragment extends Fragment {
 
@@ -21,7 +23,17 @@ public class MenuExistenciasFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+            // Recuperar el objeto "selected_product"
+            //String selectedItem = (String) getArguments().getSerializable("selected_product");
 
+            // Recuperar la cadena "articulos"
+            String encontrados = (String) getArguments().getSerializable("ART_Leidos");
+            String esperados = (String) getArguments().getSerializable("ART_Esperados");
+            String Descripcion = (String) getArguments().getSerializable("Descripcion");
+            String Clave = (String) getArguments().getSerializable("Clave");
+            Toast.makeText(getContext(), "Articulos escaneados" + encontrados, Toast.LENGTH_LONG).show();
+
+            // Ahora puedes usar 'selectedItem' y 'articulos' como necesites en este fragmento
         }
     }
 
@@ -31,6 +43,7 @@ public class MenuExistenciasFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_menu_existencias, container, false);
         AgregarInventario = root.findViewById(R.id.IB_AgregarInventario);
+        // Obtener los datos del Bundle
 
         AgregarInventario.setOnClickListener(new View.OnClickListener() {
             @Override
